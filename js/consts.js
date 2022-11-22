2f74b74fd8b522f925f6c9d3360c93a9afc1eb97
Matteo Bolovintsev <fgtzoro@icloud.com>
2022-11-23 02:43:50 +0400

    fix link2

--- js/consts.js
+++ js/consts.js
@@ -1,4 +1,4 @@
-const link0 = 'https://fgtzoro.github.io/server/db2.json/hui';
+const link0 = 'https://fgtzoro.github.io/server/db2.json';
 const link1 = 'http://localhost:3000/JK';
 const link2 = 'https://jsonplaceholder.typicode.com/users';
 
